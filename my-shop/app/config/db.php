e44c0b97d16365128a309f84f81ab9b5a0a7159e
zzzzDenzzzz <12den24@gmail.com>
2024-04-18 10:20:57 +0300

    FIX

--- my-shop/app/config/db.php
+++ my-shop/app/config/db.php
@@ -6,5 +6,5 @@ try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: ". $e->getMessage());
+    die("Connection failed: " . $e->getMessage());
 }
