4b33163042d1218e8a997c6b661b0dbe14d009c5
zzzzDenzzzz <12den24@gmail.com>
2024-04-19 20:09:23 +0300

    fix

--- my-shop/app/config/db.php
+++ my-shop/app/config/db.php
@@ -1,7 +1,9 @@
 <?php
+
 $dsn = "mysql:host=localhost;dbname=my-shop";
 $username = "root";
 $password = "";
+
 try {
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
